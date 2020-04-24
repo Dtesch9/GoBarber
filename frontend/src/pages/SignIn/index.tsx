7924dd4a2d071b52b7c5ea38a052a49639b9773b
Dtesch9 <alucard.s8123@gmail.com>
2020-04-24 19:07:20 -0300

    Hook useAuth

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../hooks/Auth';
+import { useAuth } from '../../hooks/Auth';
 import getValidationErrors from '../../utils/getValidationError';
 
 import logoImg from '../../assets/logo.svg';
@@ -22,7 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn, user } = useContext(AuthContext);
+  const { signIn, user } = useAuth();
 
   console.log(user);
 
