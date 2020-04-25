3e864d058c6109bab4370f1bb64facc677cc8a45
Dtesch9 <alucard.s8123@gmail.com>
2020-04-25 15:37:38 -0300

    Configure Routes and animation

--- frontend/src/pages/SignUp/styles.ts
+++ frontend/src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackground from '../../assets/sign-up-background.png';
@@ -18,6 +18,26 @@ export const Content = styled.div`
 
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 80px 0;
@@ -27,10 +47,27 @@ export const Content = styled.div`
     h1 {
       margin-bottom: 24px;
     }
+
+    a {
+      color: #f4ede8;
+      display: flex;
+      align-items: center;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      svg {
+        margin-right: 16px;
+      }
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
   }
 
-  a {
-    color: #f4ede8;
+  > a {
+    color: #ff9000;
     display: flex;
     align-items: center;
     margin-top: 24px;
@@ -42,7 +79,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
