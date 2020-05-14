9857bc6d65e1657568c30a8ec6e7df2a6e28c56a
Dtesch9 <alucard.s8123@gmail.com>
2020-05-14 10:57:44 -0300

    Add(@modules/**/routes): Validations

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -2,19 +2,9 @@ import { Request, Response } from 'express';
 import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentsController {
-  public async index(req: Request, res: Response): Promise<Response> {
-    const appointmentsRepository = new AppointmentsRepository();
-
-    const appointments = await appointmentsRepository.findAll();
-
-    return res.json(appointments);
-  }
-
   public async create(req: Request, res: Response): Promise<Response> {
     const user_id = req.user.id;
 
