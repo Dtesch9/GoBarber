88092c8a56c6b421a768bcf4d5a1fa3953d4f89e
Dtesch9 <alucard.s8123@gmail.com>
2020-05-25 18:56:37 -0300

    feat: Forgor / Reset password

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,11 +2,13 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: number;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
-    {children}
+    {loading ? 'Carregando...' : children}
   </Container>
 );
 
